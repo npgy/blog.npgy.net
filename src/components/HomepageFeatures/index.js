d1e1db6d4e5f443767500551e3357c5416d69beb
npgy <me@np.gy>
2022-10-02 18:14:59 -0400

    try simple solution

--- src/components/HomepageFeatures/index.js
+++ src/components/HomepageFeatures/index.js
@@ -3,76 +3,68 @@ import clsx from 'clsx';
 import styles from './styles.module.css';
 import ThemedImage from '@theme/ThemedImage';
 import useBaseUrl from '@docusaurus/useBaseUrl';
-import useThemeContext from '@theme/hooks/useThemeContext';
 
-function PolychoraImg(props) {
-  const { isDarkTheme } = useThemeContext();
-  const darkImageSrc = '../../../static/img/polychora-darkmode.png';
-  const lightImageSrc = '../../../static/img/polychora-lightmode.png';
-  return <img src={isDarkTheme ? darkImageSrc : lightImageSrc} alt="Polychora Logo" />;
-}
+// const FeatureList = [
+//   {
+//     title: 'My Developer Career',
+//     Image: require('../../../static/img/code.svg').default,
+//     description: (
+//       <>
+//         Currently employed as a Software Developer, I work with the ancient and new
+//         maintaining COBOL code on IBM mainframes, DB2 SQL, and C#. I am very interested
+//         in Docker, Kubernetes, and efficient modern code that is easily deployable and
+//         maintainable.
+//       </>
+//     ),
+//   },
+//   {
+//     title: 'Polychora Records',
+//     Image: require('../../../static/img/code.svg').default,
+//     //(
+// <ThemedImage
+//   alt="Polychora Logo"
+//   sources={{
+//     light: useBaseUrl('../../../static/img/polychora-lightmode.png'),
+//     dark: useBaseUrl('../../../static/img/polychora-darkmode.png'),
+//   }}
+// />
+//     // ),
+//     description: (
+//       <>
+//         Since 2016 I have been curating music and posting it online.
+//         This ultimately ended up turning into a record label.
+//         I run it passionately, showing the world artists who don't
+//         have a big audience, helping them focus on making the music,
+//         and not worrying about the logistics of distribution and admin.
+//         You can check it out <a target="_blank" href="https://music.polychora.co/">here.</a>
+//       </>
+//     ),
+//   },
+//   {
+//     title: 'extra',
+//     Image: require('../../../static/img/logo.svg').default,
+//     description: (
+//       <>
+//         Extend or customize your website layout by reusing React. Docusaurus can
+//         be extended while reusing the same header and footer.
+//       </>
+//     ),
+//   },
+// ];
 
-const FeatureList = [
-  // {
-  //   title: 'My Developer Career',
-  //   Image: require('../../../static/img/code.svg').default,
-  //   description: (
-  //     <>
-  //       Currently employed as a Software Developer, I work with the ancient and new
-  //       maintaining COBOL code on IBM mainframes, DB2 SQL, and C#. I am very interested
-  //       in Docker, Kubernetes, and efficient modern code that is easily deployable and
-  //       maintainable.
-  //     </>
-  //   ),
-  // },
-  {
-    title: 'Polychora Records',
-    Image: PolychoraImg(),
-    // (
-    // <ThemedImage
-    //   alt="Polychora Logo"
-    //   sources={{
-    //     light: useBaseUrl('../../../static/img/polychora-lightmode.png'),
-    //     dark: useBaseUrl('../../../static/img/polychora-darkmode.png'),
-    //   }}
-    // />
-    // ),
-    description: (
-      <>
-        Since 2016 I have been curating music and posting it online.
-        This ultimately ended up turning into a record label.
-        I run it passionately, showing the world artists who don't
-        have a big audience, helping them focus on making the music,
-        and not worrying about the logistics of distribution and admin.
-        You can check it out <a target="_blank" href="https://music.polychora.co/">here.</a>
-      </>
-    ),
-  },
-  // {
-  //   title: 'extra',
-  //   Image: require('../../../static/img/logo.svg').default,
-  //   description: (
-  //     <>
-  //       Extend or customize your website layout by reusing React. Docusaurus can
-  //       be extended while reusing the same header and footer.
-  //     </>
-  //   ),
-  // },
-];
-
-function Feature({Image, title, description}) {
-  return (
-    <div className={clsx('col col--4')}>
-      <div className="text--center">
-        <Image className={styles.featureImg} role="img" />
-      </div>
-      <div className="text--center padding-horiz--md">
-        <h3>{title}</h3>
-        <p>{description}</p>
-      </div>
-    </div>
-  );
-}
+// function Feature({Image, title, description}) {
+//   return (
+//     <div className={clsx('col col--4')}>
+//       <div className="text--center">
+//         <Image className={styles.featureImg} role="img" />
+//       </div>
+//       <div className="text--center padding-horiz--md">
+//         <h3>{title}</h3>
+//         <p>{description}</p>
+//       </div>
+//     </div>
+//   );
+// }
 
 export default function HomepageFeatures() {
   return (
@@ -83,9 +75,53 @@ export default function HomepageFeatures() {
           {/* {FeatureList.map((props, idx) => (
             <Feature key={idx} {...props} />
           ))} */}
-          <Feature Image={FeatureList[0].Image} title={FeatureList[0].title} description={FeatureList[0].description}/>
+
+          <div className={clsx('col col--4')}>
+            <div className="text--center">
+              {require('../../../static/img/code.svg').default}
+            </div>
+            <div className="text--center padding-horiz--md">
+              <h3>My Developer Career</h3>
+              <p>Currently employed as a Software Developer, I work with the ancient and new
+                maintaining COBOL code on IBM mainframes, DB2 SQL, and C#. I am very interested
+                in Docker, Kubernetes, and efficient modern code that is easily deployable and
+                maintainable.</p>
+            </div>
+          </div>
+
+          <div className={clsx('col col--4')}>
+            <div className="text--center">
+              <ThemedImage
+                alt="Polychora Logo"
+                sources={{
+                  light: useBaseUrl('../../../static/img/polychora-lightmode.png'),
+                  dark: useBaseUrl('../../../static/img/polychora-darkmode.png'),
+                }}
+              />
+            </div>
+            <div className="text--center padding-horiz--md">
+              <h3>Polychora Records</h3>
+              <p>Since 2016 I have been curating music and posting it online.
+                This ultimately ended up turning into a record label.
+                I run it passionately, showing the world artists who don't
+                have a big audience, helping them focus on making the music,
+                and not worrying about the logistics of distribution and admin.
+                You can check it out <a target="_blank" href="https://music.polychora.co/">here.</a></p>
+            </div>
+          </div>
+
+          <div className={clsx('col col--4')}>
+            <div className="text--center">
+              {require('../../../static/img/logo.svg').default}
+            </div>
+            <div className="text--center padding-horiz--md">
+              <h3>extra</h3>
+              <p>Extend or customize your website layout by reusing React. Docusaurus can
+                be extended while reusing the same header and footer.</p>
+            </div>
+          </div>
         </div>
       </div>
-    </section>
+    </section >
   );
 }
